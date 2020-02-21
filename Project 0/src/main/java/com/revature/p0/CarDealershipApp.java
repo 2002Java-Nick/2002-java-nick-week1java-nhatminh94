f00e78847389271ef0704882380e3345f6d01381
nhatminh94 <56916554+nhatminh94@users.noreply.github.com>
2020-02-21 14:01:25 -0500

    Add files via upload

---
+++ Project 0/src/main/java/com/revature/p0/CarDealershipApp.java
@@ -0,0 +1,56 @@
+package com.revature.p0;
+
+import java.util.Scanner;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class CarDealershipApp {
+	
+	private static final Logger log = Logger.getLogger(CarDealershipApp.class);
+	
+	public static void main(String[] args){
+		
+		log.info("The program has started running!");
+	
+
+		Scanner scan = new Scanner(System.in);
+		
+		
+		Profiles a = new Profiles(null, null, null, 0);
+		
+		CarLot n = new CarLot(null, 0, 0);
+		
+		while ( 1 != 0) {
+			System.out.println("\nChoose from the options below: \n1.Create Account\n2.Login"
+					+ "\n3.Quit Program");
+			int option = scan.nextInt();
+			if(option == 1) {
+				a.CreateAccount();
+			} else if (option == 2) {
+				if (a.Login()) {
+					while(1 != 0) {
+						System.out.println("\nPlease choose an option below:\n"
+								+ "\n1.View cars availabe\n2.Make an offer\n3.View your garage\n4.Quit the program");
+						int option2 = scan.nextInt();
+						if(option2 == 1) {
+							n.DeserializeCar();
+							} else if (option2 == 2) {
+								n.MakeOffer();
+							} else if (option2 == 4) {
+								System.exit(0);
+							} else if (option2 == 3) {
+								n.FillGarage();
+					
+					}
+					
+				}
+				}else {
+					System.out.println("\nLogin Failed! Try to log in again");
+				}
+			} else if (option == 3)
+				System.exit(0);
+			}
+		
+
+	}
+}
---
+++ Project 0/src/main/java/com/revature/p0/Profiles.java
@@ -0,0 +1,178 @@
+package com.revature.p0;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Profiles implements Serializable {
+	
+	private static final Logger log = Logger.getLogger(Profiles.class);
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String username;
+	private String password;
+	private String carName;
+	private int payment;
+	
+
+	
+	private String loginUser;
+	private String loginPass;
+	
+	CarLot a = new CarLot();
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+	public int getPayment() {
+		return payment;
+	}
+
+	public void setPayment(int payment) {
+		this.payment = payment;
+	}
+
+	@Override
+	public String toString() {
+		return "Profiles [username=" + username + ", password=" + password + ", carName=" + carName + ", payment="
+				+ payment + "]";
+	}
+	
+	Profiles(){
+		
+	}
+
+	Profiles(String username, String password, String carName, int payment){
+		this.username = username;
+		this.password = password;
+		this.carName = carName;
+		this.payment = payment;
+		
+	}
+	
+	
+	ArrayList<Profiles> profs = new ArrayList<Profiles>(); //List of profiles
+	
+	Profiles[] myprof = new Profiles[4]; //Fields of a profile
+
+	public void CreateAccount() {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Create an username");
+		setUsername(scan.nextLine());
+		System.out.println("Create a password");
+		setPassword(scan.nextLine());
+		System.out.println("\nAccount succesfully created!");
+		
+		profs.add(new Profiles(username, password, carName, payment));
+		SerializeAccount();
+		log.info("New account has been created");
+	}
+	
+	
+	
+	
+	
+	
+	public void SerializeAccount() {
+		  try
+	        {
+	            FileOutputStream fos = new FileOutputStream("UserProfiles.txt");
+	            ObjectOutputStream oos = new ObjectOutputStream(fos);
+	            oos.writeObject(profs);
+	            oos.close();
+	            fos.close();
+	            
+	            
+	        } catch (IOException ioe) 
+	        {
+	        	ioe.printStackTrace();
+	        } finally {
+	        	log.info("Serialization passed...information has been saved");
+	        	System.out.println("Accounts info has been saved!");
+	        }
+	
+	        }
+	 
+
+		
+	
+	public boolean Login() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What is your username?");
+		String loginUser = scan.nextLine();
+		System.out.println("What is your password?");
+		String loginPass = scan.nextLine();
+		log.info("Username and password passed into the system");
+		
+		try
+        {
+            FileInputStream fis = new FileInputStream("UserProfiles.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+ 
+            profs = (ArrayList) ois.readObject();
+ 
+            ois.close();
+            fis.close();
+        } 
+        catch (IOException ioe) 
+        {
+            ioe.printStackTrace();
+            return false;
+        } 
+        catch (ClassNotFoundException c) 
+        {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return false;
+        }
+		
+		if(profs.toString().contains(loginUser) && profs.toString().contains(loginPass)){
+			log.info("Account credentials succesfully verified...Logging in");
+			return true;
+		} else if(loginUser.equals("employee") && loginPass.equals("password")) {
+			log.info("Employee credentials succesfully verified..Logging in as an employee");
+			a.employeeOptions();
+		} else {
+			return false;
+		}
+		return false;
+	}
+}
+
+
